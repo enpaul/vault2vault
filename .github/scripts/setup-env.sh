9c6486ce5577a6da9bd8125aa3a3aabee3bc1845
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2023-03-29 18:46:57 -0400

    Update poetry CI version to 1.4.1
    Update CI env script to move poetry version to main CI workflow config

--- .github/scripts/setup-env.sh
+++ .github/scripts/setup-env.sh
@@ -4,11 +4,13 @@
 # to create a repeatable local environment for tests to be run in. The python env
 # this script creates can be accessed at the location defined by the CI_VENV variable
 # below.
+#
+# POETRY_VERSION can be set to install a specific version of Poetry
 
 set -e;
 
 CI_CACHE=$HOME/.cache;
-POETRY_VERSION=1.3.2;
+INSTALL_POETRY_VERSION="${POETRY_VERSION:-1.3.2}";
 
 mkdir --parents "$CI_CACHE";
 
@@ -20,7 +22,7 @@ curl --location https://install.python-poetry.org \
   --silent \
   --show-error;
 python "$CI_CACHE/install-poetry.py" \
-  --version "$POETRY_VERSION" \
+  --version "$INSTALL_POETRY_VERSION" \
   --yes;
 poetry --version --no-ansi;
 poetry run pip --version;
